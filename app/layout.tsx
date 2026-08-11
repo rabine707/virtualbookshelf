905af19071225f735050b5f8abb38758b787ff96
rabine707 <krabinejr@gmail.com>
2026-08-10 22:14:09 -0700

    Load modal scroll containment styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,7 @@ import "./theme-packs.css";
 import "./theme-environment-pack.css";
 import "./theme-gallery.css";
 import "./modal-cover-hotfix.css";
+import "./modal-scroll-lock.css";
 import "./web-cover-search.css";
 import "./saved-cover-choices.css";
 import "./generated-spines.css";
