dc8d8194dfc1434fa5e362844bb01d0589dc44a3
rabine707 <krabinejr@gmail.com>
2026-08-10 18:06:20 -0700

    Add SPINES comparison test page

---
+++ app/scan/spines/page.tsx
@@ -0,0 +1,9 @@
+import SpinesShelfTest from "./SpinesShelfTest";
+
+export const metadata = {
+  title: "SPINES Shelf Test · Shelf of Fame",
+};
+
+export default function SpinesShelfTestPage() {
+  return <SpinesShelfTest />;
+}
