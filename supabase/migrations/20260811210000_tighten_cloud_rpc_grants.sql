960d3a95a76de623ba2e10436ead8dbff91dc164
rabine707 <krabinejr@gmail.com>
2026-08-11 13:42:24 -0700

    Restrict private cloud RPCs to signed-in users

---
+++ supabase/migrations/20260811210000_tighten_cloud_rpc_grants.sql
@@ -0,0 +1,3 @@
+revoke execute on function public.get_my_shelf() from anon;
+revoke execute on function public.sync_my_shelf(jsonb,jsonb,boolean) from anon;
+revoke execute on function public.update_my_shelf_settings(jsonb) from anon;
