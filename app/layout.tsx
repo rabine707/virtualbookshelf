f47e76ce9372f98ade71248676e2a2f0425104fe
rabine707 <krabinejr@gmail.com>
2026-08-12 16:01:47 -0700

    Load cinematic botanical lighting layer

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,6 +36,7 @@ import "./shelf-art-polish.css";
 import "./botanical-v3.css";
 import "./botanical-room-v2-fix.css";
 import "./botanical-modular-shelves.css";
+import "./botanical-cinematic-lighting.css";
 import "./bookshelf-3d-prototype.css";
 import "./prototype-page.css";
 import ClientEnhancers from "./ClientEnhancers";
