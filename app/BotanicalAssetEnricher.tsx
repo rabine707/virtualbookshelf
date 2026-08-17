c542256c9c1ddb4fc41c4da75463a4f2dc9c1a8a
rabine707 <krabinejr@gmail.com>
2026-08-17 11:49:10 -0700

    Make Botanical feel like a lived-in reading room
    * Dress Botanical room with scene props
    
    * Add Botanical wonderland environment pass
    
    * Wire Botanical wonderland environment styles

--- app/BotanicalAssetEnricher.tsx
+++ app/BotanicalAssetEnricher.tsx
@@ -6,6 +6,7 @@ import { createPortal } from "react-dom";
 
 const ROOM_PROPS = {
   sconce: "https://cdn.polyhaven.com/asset_img/thumbs/industrial_wall_sconce.png?format=png",
+  frame: "https://cdn.polyhaven.com/asset_img/thumbs/fancy_picture_frame_02.png?format=png",
 } as const;
 
 function isBotanical() {
@@ -50,10 +51,69 @@ export default function BotanicalAssetEnricher() {
           alt=""
           fill
           priority
-          sizes="(max-width: 760px) 250px, (max-width: 1240px) 355px, 470px"
+          sizes="(max-width: 760px) 280px, (max-width: 1240px) 390px, 560px"
           style={{ objectFit: "contain", objectPosition: "left top" }}
         />
       </span>
+
+      <span className="botanical-scene-vine botanical-scene-vine-left" style={{ aspectRatio: "2 / 5" }}>
+        <Image
+          src="/themes/botanical/v3/hanging-vine-right.webp"
+          alt=""
+          fill
+          priority
+          sizes="180px"
+          style={{ objectFit: "contain", objectPosition: "top right" }}
+        />
+      </span>
+
+      <span className="botanical-scene-vine botanical-scene-vine-right" style={{ aspectRatio: "2 / 5" }}>
+        <Image
+          src="/themes/botanical/v3/hanging-vine-right.webp"
+          alt=""
+          fill
+          priority
+          sizes="180px"
+          style={{ objectFit: "contain", objectPosition: "top right" }}
+        />
+      </span>
+
+      <span className="botanical-scene-planter" style={{ aspectRatio: "1 / 1" }}>
+        <Image
+          src="/themes/botanical/v3/ceramic-pothos-planter.webp"
+          alt=""
+          fill
+          priority
+          sizes="150px"
+          style={{ objectFit: "contain", objectPosition: "center bottom" }}
+        />
+      </span>
+
+      <div className="botanical-scene-art">
+        <span className="botanical-scene-art-paper">
+          <i className="botanical-art-stem botanical-art-stem-a" />
+          <i className="botanical-art-stem botanical-art-stem-b" />
+          <i className="botanical-art-mushroom botanical-art-mushroom-a" />
+          <i className="botanical-art-mushroom botanical-art-mushroom-b" />
+          <i className="botanical-art-leaf botanical-art-leaf-a" />
+          <i className="botanical-art-leaf botanical-art-leaf-b" />
+        </span>
+        <img className="botanical-scene-art-frame" src={ROOM_PROPS.frame} alt="" decoding="async" />
+      </div>
+
+      <div className="botanical-scene-antique-books">
+        <span />
+        <span />
+        <span />
+      </div>
+
+      <div className="botanical-scene-candle" aria-hidden="true">
+        <span className="botanical-scene-candle-glow" />
+        <span className="botanical-scene-candle-flame" />
+        <span className="botanical-scene-candle-wick" />
+        <span className="botanical-scene-candle-wax" />
+        <span className="botanical-scene-candle-holder" />
+      </div>
 
       <span className="botanical-real-sconce-glow" />
       <img
@@ -69,8 +129,8 @@ export default function BotanicalAssetEnricher() {
           alt=""
           fill
           priority
-          sizes="(max-width: 1240px) 430px, 540px"
-          style={{ objectFit: "contain", objectPosition: "right bottom" }}
+          sizes="(max-width: 1240px) 470px, 650px"
+          style={{ objectFit: "contain", objectPosition: "left bottom" }}
         />
       </span>
     </div>,
