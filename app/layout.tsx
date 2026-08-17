3aac0075e0f25ceebdfbf7ba0c1e3d24ce07c7ea
rabine707 <krabinejr@gmail.com>
2026-08-17 09:14:55 -0700

    Load compact cinematic breakpoint bridge

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,6 +48,7 @@ import "./cinematic-library-v8.css";
 import "./cinematic-library-v9.css";
 import "./cinematic-library-v10.css";
 import "./cinematic-library-v11.css";
+import "./cinematic-library-v12.css";
 import BotanicalAssetEnricher from "./BotanicalAssetEnricher";
 import BotanicalPropStyles from "./BotanicalPropStyles";
 import BotanicalLampFixStyles from "./BotanicalLampFixStyles";
